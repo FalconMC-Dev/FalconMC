8a2bf8997eb5078f14741f259d3fd91c26803b4c
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2022-03-26 23:39:06 +0100

    Excluded versions now also affect joining the game

--- crates/default_protocol/src/serverbound/v1_8_9/login.rs
+++ crates/default_protocol/src/serverbound/v1_8_9/login.rs
@@ -2,8 +2,10 @@ pub use inner::*;
 
 #[falcon_default_protocol_derive::packet_module]
 mod inner {
+    use mc_chat::{ChatColor, ChatComponent, ComponentStyle};
     use falcon_core::network::connection::MinecraftConnection;
     use falcon_core::network::packet::{PacketDecode, PacketHandler, TaskScheduleResult};
+    use falcon_core::server::config::FalconConfig;
     use falcon_core::server::ServerActor;
 
     #[derive(PacketDecode)]
@@ -15,9 +17,16 @@ mod inner {
     impl PacketHandler for LoginStartPacket {
         fn handle_packet(self, connection: &mut dyn MinecraftConnection) -> TaskScheduleResult {
             let version = connection.handler_state().protocol_id();
-            let wrapper = connection.wrapper();
-            connection.server()
-                .player_login(self.name, version, wrapper);
+            if FalconConfig::global().excluded_versions().contains(&version.unsigned_abs()) {
+                connection.disconnect(ChatComponent::from_text(
+                    "Disabled version",
+                    ComponentStyle::with_version(version.unsigned_abs()).color_if_absent(ChatColor::Red)
+                ));
+            } else {
+                let wrapper = connection.wrapper();
+                connection.server()
+                    .player_login(self.name, version, wrapper);
+            }
             Ok(())
         }
 
