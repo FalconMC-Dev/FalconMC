4d8211c1c48fbb5bb8a8df2614e02f39ba6ab40c
taylorsk <taylrr@protonmail.com>
2022-08-26 17:30:14 -0700

    fixes issue #8

--- crates/main/src/server/mod.rs
+++ crates/main/src/server/mod.rs
@@ -2,6 +2,7 @@ use std::io::Read;
 use std::thread;
 
 use anyhow::{Context, Result};
+use falcon_core::server::config::FalconConfig;
 use falcon_logic::server::ServerWrapper;
 use flate2::read::GzDecoder;
 use tokio::sync::mpsc::unbounded_channel;
@@ -9,6 +10,7 @@ use tokio::sync::mpsc::unbounded_channel;
 use falcon_core::schematic::{SchematicData, SchematicVersionedRaw};
 use falcon_core::ShutdownHandle;
 use falcon_logic::{FalconServer, FalconWorld};
+
 use tracing::{info, debug};
 
 use crate::network::NetworkListener;
@@ -19,24 +21,29 @@ pub mod console;
 pub(crate) fn start_server(shutdown_handle: ShutdownHandle) -> Result<()> {
     info!("Starting server thread...");
 
-    let world = {
-        let world_file = std::fs::read("./world.schem")
-            .with_context(|| "Could not load `world.schem`, stopping launch")?;
-        let mut gz = GzDecoder::new(&world_file[..]);
-        let mut decompressed_world = Vec::new();
-        gz.read_to_end(&mut decompressed_world)
-            .with_context(|| "Could not decompress world.schem, is this a valid schematic?")?;
-        debug!("Checkpoint - loaded schem file");
-        let schematic: SchematicVersionedRaw = fastnbt::from_bytes(&decompressed_world)
-            .with_context(|| "Could not parse schematic file, is this valid nbt?")?;
-        debug!("Checkpoint - deserialized into raw format");
-        let data = SchematicData::try_from(schematic).with_context(|| {
-            "Invalid schematic, this server cannot use this schematic currently!"
-        })?;
-        debug!("Checkpoint - parsed raw format");
-        FalconWorld::try_from(data)?
+    let world = match FalconConfig::global().world_file() {
+        Some(world_file) => { 
+            let world_file = std::fs::read(world_file)
+                .with_context(|| "Could not load world schematic, stopping launch")?;
+            let mut gz = GzDecoder::new(&world_file[..]);
+            let mut decompressed_world = Vec::new();
+            gz.read_to_end(&mut decompressed_world)
+                .with_context(|| "Could not decompress world.schem, is this a valid schematic?")?;
+            debug!("Checkpoint - loaded schem file");
+            let schematic: SchematicVersionedRaw = fastnbt::from_bytes(&decompressed_world)
+                .with_context(|| "Could not parse schematic file, is this valid nbt?")?;
+            debug!("Checkpoint - deserialized into raw format");
+            let data = SchematicData::try_from(schematic).with_context(|| {
+                "Invalid schematic, this server cannot use this schematic currently!"
+            })?;
+            
+            debug!("Checkpoint - parsed raw format");
+            info!("Loaded world");
+
+            FalconWorld::try_from(data)?
+        }
+        None => FalconWorld::new(0, 0, 0, 0, 0),
     };
-    info!("Loaded world");
 
     let console_rx = ConsoleListener::start_console(shutdown_handle.clone())?;
     let (server_tx, server_rx) = unbounded_channel();
