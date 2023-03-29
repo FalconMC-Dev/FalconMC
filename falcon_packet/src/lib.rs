dc157ef87a4d6bc78e91d80cee19cc3e167b1cb6
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-03-29 11:15:05 +0200

    chore(packets): More packet traits
    Turns out we do need the seed versions.

--- falcon_packet/src/lib.rs
+++ falcon_packet/src/lib.rs
@@ -6,11 +6,18 @@
 //! The design philosophy of this crate is inspired by [serde](https://serde.rs).
 //!
 //! ## **Traits**
-//! Three traits are introduced:
+//! Six traits are introduced:
 //! - [`PacketRead`]: How to read a type from the network.
 //! - [`PacketWrite`]: How to write a type to the network.
 //! - [`PacketSize`]: Memory-efficient size computation of the data
 //!     when it would be written to the netork.
+//! - [`PacketReadSeed`]: How to read a type from the network.
+//!     This trait is used to pass data to the read implemenetation.
+//! - [`PacketWriteSeed`]: How to write a type to the network.
+//!     This trait is used to pass data to the write implemenetation.
+//! - [`PacketSizeSeed`]: Memory-efficient size computation of the data
+//!     when it would be written to the netork.
+//!     This trait is used to pass data to the size implemenetation.
 //!
 //! Because [Minecraft's protocol](https://wiki.vg/) doesn't
 //! translate one-to-one to Rust types, this crate offers some
@@ -27,7 +34,6 @@
 // //! - [`PacketRead`](falcon_packet_core_derive::PacketRead)
 // //! - [`PacketWrite`](falcon_packet_core_derive::PacketWrite)
 // //! - [`PacketSize`](falcon_packet_core_derive::PacketSize)
-//!
 
 use bytes::{Buf, BufMut};
 
@@ -84,6 +90,65 @@ pub trait PacketSize {
     /// # Implementors
     /// It is highly encouraged to optimize this function.
     /// Avoid writing the type to a buffer and returning
-    /// that buffer's length at all cost.
+    /// that buffer's change in length at all cost.
     fn size(&self) -> usize;
 }
+
+/// A data structure that can read another data type from a minecraft
+/// connection, see [`DeserializeSeed`](https://docs.rs/serde/latest/serde/de/trait.DeserializeSeed.html).
+///
+/// This trait should rarely be implemented manually, if you implement this for
+/// a general type, please contribute it to this project.
+pub trait PacketReadSeed {
+    /// The type produced by using this seed.
+    type Value;
+
+    /// This function extracts the type from the given buffer.
+    ///
+    /// # Important
+    /// Implementations that read directly from the buffer
+    /// (no redirection of this function/trait) **must ensure**
+    /// that the remaining length of the buffer is always
+    /// checked first before reading bytes from it.
+    /// This is to eliminate panics.
+    fn read<B>(self, buffer: &mut B) -> Result<Self::Value, ReadError>
+    where
+        B: Buf + ?Sized;
+}
+
+/// A data structure that can write another data type from a minecraft
+/// connection, see [`SeralizeSeed`](https://docs.rs/serde/latest/serde/ser/trait.SerializeSeed.html).
+///
+/// This trait should rarely be implemented manually, if you implement this for
+/// a general type, please contribute it to this project.
+pub trait PacketWriteSeed<'a> {
+    /// The type written by using this seed.
+    type Value;
+
+    /// This function serializes the type to the given buffer.
+    fn write<B>(self, value: &'a Self::Value, buffer: &'a mut B) -> Result<(), WriteError>
+    where
+        B: BufMut + ?Sized;
+}
+
+/// A data structure that can efficiently compute
+/// its serialized size on the network buffer.
+/// It is a stateful variant of [`PacketSize`], see
+/// [`SerializeSeed`](https://docs.rs/serde/latest/serde/de/trait.DeserializeSeed.html).
+///
+/// This trait should rarely be implemented manually, if you implement this for
+/// a general type, please contribute it to this project.
+pub trait PacketSizeSeed<'a> {
+    /// The type measured by using this seed.
+    type Value;
+
+    /// This function computes the exact network
+    /// size of the type.
+    ///
+    /// # Implementors
+    /// It is highly encouraged to optimize this function.
+    /// Avoid writing the type to a buffer and returning
+    /// that buffer's change in length at all cost.
+    fn size(self, value: &'a Self::Value) -> usize;
+}
+
