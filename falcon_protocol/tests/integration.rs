b237a6d112b2fe453b7643fce18cac0693ea7815
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-05-05 10:23:34 +0200

    test(protocol): packets! macro integration test

--- falcon_protocol/tests/integration.rs
+++ falcon_protocol/tests/integration.rs
@@ -1,3 +1,4 @@
+use bytes::Bytes;
 use falcon_packet::packet;
 use falcon_protocol::packets;
 
@@ -16,4 +17,16 @@ packet! {
 packets! {
     ExamplePacket: { 47 = 0x01 },
     PacketTwo: { 47 = 0x03; 48 = 0x02 },
+}
+
+#[test]
+fn test_switching() {
+    let mut buffer = Bytes::from_static(&[0, 0, 0, 1]);
+    let packet = read_packet(&mut buffer, 1, 48).unwrap();
+    assert!(packet.is_none());
+    let packet = read_packet(&mut buffer, 1, 47).unwrap();
+    assert!(packet.is_some());
+    let packet = packet.unwrap();
+    assert!(packet.is::<ExamplePacket>());
+    assert_eq!(1, packet.downcast_ref::<ExamplePacket>().unwrap().x);
 }
