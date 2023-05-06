75bbca49f524730ba0cdf80184438d41953aa705
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-05-06 18:16:29 +0200

    feat(packets): less poisoned errors

--- falcon_protocol/falcon_protocol_core/src/lib.rs
+++ falcon_protocol/falcon_protocol_core/src/lib.rs
@@ -4,7 +4,8 @@ use check::VersionMappings;
 use data::parse_mappings;
 use generate::generate_output;
 use proc_macro2::TokenStream;
-use proc_macro_error::abort_if_dirty;
+use proc_macro_error::{abort, abort_if_dirty, set_dummy};
+use quote::quote;
 use syn::parse::Parser;
 
 mod check;
@@ -13,9 +14,18 @@ mod generate;
 mod tests;
 
 pub fn protocol_core(input: TokenStream) -> TokenStream {
+    set_dummy(quote! {
+        pub fn read_packet<B>(buffer: &mut B, packet_id: i32, protocol_version: i32) -> ::std::result::Result<std::option::Option<Box<dyn ::falcon_protocol::Packet>>, ::falcon_packet::ReadError>
+        where
+            B: ::bytes::Buf,
+        {
+            unimplemented!()
+        }
+    });
+
     let syntax = match parse_mappings.parse2(input) {
         Ok(syntax) => syntax,
-        Err(error) => return error.to_compile_error(),
+        Err(error) => abort!(error.span(), error.to_string()),
     };
 
     let mappings = VersionMappings::validate_mappings(syntax);
